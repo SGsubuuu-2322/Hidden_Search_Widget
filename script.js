8b31889e5ad8bcf9ef767c9a24db7e56b58f24f7
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-10-26 23:01:28 +0530

    Added some styling and action through css and js...

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 const input = document.querySelector(".input");
+const bodyElement = document.body;
 
 btn.addEventListener("click", () => {
   search.classList.toggle("active");
+  bodyElement.classList.toggle("act");
   input.focus();
 });
