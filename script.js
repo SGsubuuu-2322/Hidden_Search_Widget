364f5012f22ee804b3f553adb29c062d72aaa8d8
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-10-30 23:05:45 +0530

    Added the meaningful comments in the script file...

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// These are the constants containing all important HTML elements for further modification...
 const search = document.querySelector(".search");
 const btn = document.querySelector(".btn");
 const input = document.querySelector(".input");
 const bodyElement = document.body;
 
+
+// This is the event listener for that search button
 btn.addEventListener("click", () => {
   search.classList.toggle("active");
   bodyElement.classList.toggle("act");
